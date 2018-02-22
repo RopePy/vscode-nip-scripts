17e6b1838b8bbe8eda6ed1ba4a65ce2893aa7828
traBpUkciP <scott.doucet@hotmail.com>
2018-02-22 01:26:10 -0400

    Added nipfile intellisense
    - auto complete common script setups

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from 'vscode';
-import { Terminal, MessageItem } from 'vscode';
+import { Terminal, MessageItem, DocumentSelector } from 'vscode';
 import { ScriptNodeProvider } from './nipScripts'
 
 export function activate(context: vscode.ExtensionContext) {
@@ -9,7 +9,26 @@ export function activate(context: vscode.ExtensionContext) {
 	const nipScriptsProvider = new ScriptNodeProvider(rootPath);
 
 	vscode.window.registerTreeDataProvider('nipScripts', nipScriptsProvider);
-	vscode.window.onDidCloseTerminal(term => terminalMap.delete(term.name));
+    vscode.window.onDidCloseTerminal(term => terminalMap.delete(term.name));
+    
+	// The most simple completion item provider which 
+	// registers for text files (`'plaintext'`), and
+	// return the 'Hello World' and 
+    //   a snippet-based completion item.
+    let docSelector: DocumentSelector = [{ language: 'json', pattern: '**/nip.json' }]
+	vscode.languages.registerCompletionItemProvider(docSelector, {
+		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken) {
+			return [
+                createKeyValueSnippetItem('"scripts"', '"scripts": {\n\t"${1|start,test,build,publish|}": "${2}"\n}')
+			];
+		}
+    });
+
+    function createKeyValueSnippetItem(hint: string, snippet: string): vscode.CompletionItem {
+        let item = new vscode.CompletionItem(hint, vscode.CompletionItemKind.Snippet);
+        item.insertText = new vscode.SnippetString(snippet);
+		return item;
+    }
 
 	vscode.commands.registerCommand('nipScripts.executeCommand', task => {
 		const packageManager = vscode.workspace.getConfiguration('nip').get('packageManager') || 'nip';
